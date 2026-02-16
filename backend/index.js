f8e1b0c13f0ee5438e32e8921176196a7e1a3d69
pradeesh <pradeeshmathaswaran@gmail.com>
2026-02-17 01:44:01 +0530

    Fix: Use path.resolve for SPA fallback

--- backend/index.js
+++ backend/index.js
@@ -127,7 +127,7 @@ app.delete('/api/employee/:id', async (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
